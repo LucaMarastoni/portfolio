7112c4c873044148e8ba9245cb9e88c41bb4909c
LucaMarastoni <marastoni311@gmail.com>
2025-02-23 11:30:50 +0100

    Remove commented-out code and clean up CSS styles for improved readability

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const overlay = document.getElementById('overlay');
     const closePrivacyCard = document.getElementById('closePrivacyCard');
 
-    // Apre la card della Privacy Policy
     privacyBtn.addEventListener('click', function(event) {
         event.preventDefault();
         privacyCard.classList.add('show-card');
         overlay.style.display = 'block';
     });
 
-    // Chiude la card della Privacy Policy cliccando su "Chiudi"
     closePrivacyCard.addEventListener('click', function() {
         privacyCard.classList.remove('show-card');
         privacyCard.classList.add('hide-card');
@@ -21,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 300);
     });
 
-    // Chiude la card della Privacy Policy cliccando fuori dalla card (overlay)
     overlay.addEventListener('click', function() {
         privacyCard.classList.remove('show-card');
         privacyCard.classList.add('hide-card');
